2cffedf2d81b0584c031cecca4bf071125220138
Eduardo Sandoval <ed.sand.pol@gmail.com>
2020-11-11 21:30:10 -0700

    reorganice el codigo y agregue una lista de cosas a realizar

---
+++ QUEHACER.md
@@ -0,0 +1,9 @@
+* Agregar un icono a la pagina
+* Crear base de datos para guardar las notas
+* Mostrar los titulos de las notas en el apartado de la izquierda
+* Mostrar el titulo de cada nota en el apartado superior derecho
+* Mostrar el contenido de cada nota en el apartado de texto derecho
+* Implementar el añadir una nota
+* Implementar el borrar la nota
+* Implementar el guardar la nota que añadiste
+* Pasarlo a escritorio usando NodeJS y Electron
---
+++ codigo/logica.js
@@ -0,0 +1,28 @@
+// Para conectarme con los botones
+const btnNuevaNota = document.getElementById('btnNuevaNota');
+const btnBorrar = document.getElementById('btnBorrar');
+const btnGuardar = document.getElementById('btnGuardar');
+
+// Para conectarme con los elementos de las notas
+const listaNotas = document.getElementById('listaNotas');
+const tituloNota = document.getElementById('tituloNota');
+const areaTxt = document.getElementById('areaTxt');
+
+
+function inicializar() {
+	alert('inicializando...');
+}
+
+btnNuevaNota.onclick = function crearNota() {
+	alert('Falta implementar la funcion: Crear nota');
+}
+
+btnBorrar.onclick = function borrarNota() {
+	alert('Falta implementar la funcion: Borrar nota');
+}
+
+btnGuardar.onclick = function guardarNota() {
+	alert('Falta implementar la funcion: Guardar nota');
+}
+
+inicializar();
